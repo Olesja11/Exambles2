0c07cfb76f3ed314a416e39aec130ad2c8456746
Alesya Boyarina <l_i_s_a11@mail.ru>
2022-08-29 19:32:56 +0300

    Add Ex002

---
+++ Ex002/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.WriteLine("Введи число");
+int num = int.Parse(Console.ReadLine());
+int i = 0;
+int chislo = num;
+while (chislo > 0)
+{
+    chislo = chislo/10;
+    i = i+1;
+}
+if (i<3) {
+   Console.WriteLine("Третьей цифры нет"); 
+}
+else{
+    while (i > 3) 
+    {
+        num = num/10;
+        i = i - 1;
+    }
+    Console.WriteLine(num%10);
+}
+
+
